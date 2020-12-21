6ee6337cb6b4b7e3f2d83fab7d8dd23325c60dc8
Nikita Naleikin <trubatvoia3@gmail.com>
2020-12-21 21:26:46 +0400

    Adds a promo for the "Testimonials" section

--- src/components/mainContentSections/Testimonials/Testimonials.jsx
+++ src/components/mainContentSections/Testimonials/Testimonials.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import MainContentSectionPromo from '../MainContentSectionPromo/MainContentSectionPromo';
+
 const Testimonials = () => (
-  <div className="main-content-section">
-    <div className="wrapper">Testimonials</div>
-  </div>
+  <section>
+    <MainContentSectionPromo
+      sectionName="testimonials"
+      titleRowOne="Testimonials"
+      titleRowTwo="Happy clients"
+      bgPosition="center 70%"
+    />
+  </section>
 );
 
 export default Testimonials;
