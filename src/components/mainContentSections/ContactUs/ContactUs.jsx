fdc206723503ce80e6b4cc180fc4aec0f92425f3
Nikita Naleikin <trubatvoia3@gmail.com>
2020-12-27 21:24:15 +0400

    Adds a promo for the "Contact us" section

--- src/components/mainContentSections/ContactUs/ContactUs.jsx
+++ src/components/mainContentSections/ContactUs/ContactUs.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import MainContentSectionPromo from '../MainContentSectionPromo/MainContentSectionPromo';
+
 const ContactUs = () => (
-  <div className="main-content-section">
-    <div className="wrapper">Contact us</div>
-  </div>
+  <section>
+    <MainContentSectionPromo
+      sectionName="contact-us"
+      titleRowOne="Contact"
+      titleRowTwo="Us"
+      bgPosition="center 50%"
+    />
+  </section>
 );
 
 export default ContactUs;
