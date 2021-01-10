3d1a31dd92f23c5a1124269b9e31d042d4e2dd8e
Nikita Naleikin <trubatvoia3@gmail.com>
2021-01-10 12:54:55 +0400

    Adds a promo for the "Blog" section

--- src/components/mainContentSections/Blog/Blog.tsx
+++ src/components/mainContentSections/Blog/Blog.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 import { MainContentSectionPromo } from '../MainContentSectionPromo/MainContentSectionPromo';
 
-const AboutUs: React.FC = () => <section />;
+const AboutUs: React.FC = () => (
+  <section>
+    <MainContentSectionPromo
+      sectionName="blog"
+      titleRowOne="Blog"
+      titleRowTwo="Page"
+      bgPosition="center 80%"
+    />
+  </section>
+);
 
 export default AboutUs;
